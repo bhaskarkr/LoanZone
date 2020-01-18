5f641df6f81707ed3116eb517ceabdf83e9e3468
Bhaskar Kumar <bhaskar.kumar@phonepe.com>
2020-01-19 00:09:00 +0530

    signup done

--- loan-zone/src/components/signup/signup.js
+++ loan-zone/src/components/signup/signup.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
@@ -6,6 +6,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import axios from 'axios';
 
 const useStyles = makeStyles({
     card: {
@@ -38,12 +39,13 @@ export default function SignUp() {
     const [gstin, setGstin] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
+    const [dob, setDob] = useState('');
     const [done, setDone] = useState(false);
     const handleNameChange = (event) => {
         setName(event.target.value);
     }
     const handlePanChange = (event) => {
-        setPan(event.target.value);
+        setPan(event.target.files[0]);
     }
     const handleAadhaarChange = (event) => {
         setAadhaar(event.target.value);
@@ -57,31 +59,66 @@ export default function SignUp() {
     const handleConfirmPasswordChange = (event) => {
         setConfirmPassword(event.target.value);
     }
+    const handleDobChange = (event) => {
+        setConfirmPassword(event.target.value);
+    }
     const handleSignUp = (event) => {
+
+        if(password !== confirmPassword)
+        {
+            alert("password mismatch")
+        }
         setDone(true);
+        let signUpPayload = {
+            name: name,
+            pan: pan,
+            aadhaar: aadhaar,
+            gstin: gstin,
+            password: password,
+            dob: dob
+        }
+        var formData = new FormData();
+        formData.append('name',name);
+        formData.append('pan',pan);
+        formData.append('aadhaar',aadhaar);
+        formData.append('gstin',gstin);
+        formData.append('password',password);
+        console.log(formData.get('pan'));
+        axios.post("http://192.168.100.56:5244/register", { data: formData })
+            .then(response=>{
+                console.log(response);
+                setDone(false);
+            })
+            .catch(err=>{
+                setDone(false);
+                console.log(err);
+                alert("Server Issue");
+            })
     }
 
     return (
         <Card className={classes.card} elevation="15">
-           
-                <CardMedia
-                    className={classes.media}
-                    image="https://www.societegenerale.com/sites/default/files/styles/newsroom_grille/public/logo-societe-generale11_12.jpg?itok=HAPSB8u1"
-                    title="Company"
-                />
-                <CardContent>
-                    <form className={classes.forms} noValidate autoComplete="off">
-                        <TextField className={classes.inputField} id="outlined-error" label="Name" variant="outlined" value={name} placeholder="Enter your Name" onChange={handleNameChange} required /><br /><br />
-                        <TextField className={classes.inputField} id="outlined-error" label="PAN" variant="outlined" placeholder="Enter your PAN" onChange={handlePanChange} required /><br /><br />
-                        <TextField className={classes.inputField} id="outlined-error" label="Aadhaar" variant="outlined" placeholder="Enter your Aadhar" onChange={handleAadhaarChange} required /><br /><br />
-                        <TextField className={classes.inputField} id="outlined-error" label="GSTIN" variant="outlined" placeholder="Enter your GSTIN" onChange={handleGstinChange} required /><br /><br />
-                        <TextField className={classes.inputField} id="outlined-error" type="password" label="Password" variant="outlined" placeholder="Enter your Password" onChange={handlePasswordChange} required /><br /><br />
-                        <TextField className={classes.inputField} id="outlined-error" type="password" label="Confirm Password" variant="outlined" placeholder="Enter your Password Again" onChange={handleConfirmPasswordChange} required /><br /><br />
-                    </form>
-                </CardContent>
-            
+
+            <CardMedia
+                className={classes.media}
+                image="https://www.societegenerale.com/sites/default/files/styles/newsroom_grille/public/logo-societe-generale11_12.jpg?itok=HAPSB8u1"
+                title="Company"
+            />
+            <CardContent>
+                {/* {passwordMismatch} */}
+                <form className={classes.forms} noValidate autoComplete="off">
+                    <TextField className={classes.inputField} id="outlined-error" label="Name" variant="outlined" value={name} placeholder="Enter your Name" onChange={handleNameChange}  /><br /><br />
+                    {/* <TextField className={classes.inputField} id="outlined-error" label="DOB" variant="outlined" placeholder="Enter your Date Of Birth" onChange={handleDobChange}  /><br /><br /> */}
+                    <TextField className={classes.inputField} id="outlined-error" type="file" label="PAN"  placeholder="Enter your PAN" onChange={handlePanChange}  /><br /><br />
+                    <TextField className={classes.inputField} id="outlined-error"  type="file"label="Aadhaar"  placeholder="Enter your Aadhar" onChange={handleAadhaarChange}  /><br /><br />
+                    <TextField className={classes.inputField} id="outlined-error" label="GSTIN" variant="outlined" placeholder="Enter your GSTIN" onChange={handleGstinChange}  /><br /><br />
+                    <TextField className={classes.inputField} id="outlined-error" type="password" label="Password" variant="outlined" placeholder="Enter your Password" onChange={handlePasswordChange}  /><br /><br />
+                    <TextField className={classes.inputField} id="outlined-error" type="password" label="Confirm Password" variant="outlined" placeholder="Enter your Password Again" onChange={handleConfirmPasswordChange}  /><br /><br />
+                </form>
+            </CardContent>
+
             <CardActions>
-                <Button className={classes.cardButton} size="large" color="secondary" elevation="15" onClick={handleSignUp}>
+                <Button className={classes.cardButton} size="large" color="secondary" elevation="15" onClick={handleSignUp} disabled={done} >
                     SignUp
                 </Button>
             </CardActions>
