e509cd6b61412cc5c3abb84909bdd6a414b16c04
Bhaskar Kumar <bhaskar.kumar@phonepe.com>
2020-01-18 22:28:49 +0530

    signup

--- loan-zone/src/App.js
+++ loan-zone/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import MediaCard from './components/signup/signup'
+import SignUp from './components/signup/signup'
 
 function App() {
   return (
     <div className="App">
       <p>test</p>
-      <MediaCard />
+
+        <SignUp />
+
     </div>
   );
 }
--- loan-zone/src/components/signup/signup.js
+++ loan-zone/src/components/signup/signup.js
@@ -6,46 +6,67 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles({
-  card: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 140,
-  },
+    card: {
+        maxWidth: 445,
+        maxHeight: '80%',
+        marginLeft: '40%',
+        marginTop: 200,
+        background: 'white'
+    },
+    media: {
+        height: 100,
+    },
+    cardButton: {
+        marginLeft: '25%',
+        border: 10,
+        borderWidth: 3,
+        minWidth: 180
+    },
+    inputField: {
+        width: '80%'
+    }
+
 });
 
-export default function MediaCard() {
-  const classes = useStyles();
+export default function SignUp() {
+    const classes = useStyles();
+    const state = {
+        name: "",
+        pan: "",
+        aadhar: "",
+        gstin: ""
+    }
+    const handleTextChange = (event) => {
+        console.log(event);
+    }
 
-  return (
-    <Card className={classes.card}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>
-  );
+    return (
+        <Card className={classes.card}>
+            <CardActionArea>
+                <CardMedia
+                    className={classes.media}
+                    image="https://www.societegenerale.com/sites/default/files/styles/newsroom_grille/public/logo-societe-generale11_12.jpg?itok=HAPSB8u1"
+                    title="Company"
+                />
+                <CardContent>
+                    <form className={classes.forms} noValidate autoComplete="off">
+                        <TextField className={classes.inputField} id="outlined-error" label="Name" variant="outlined" value={state.name} placeholder="Enter your Name" onChange={handleTextChange} required /><br /><br />
+                        <TextField className={classes.inputField} id="outlined-error" label="PAN" variant="outlined" placeholder="Enter your PAN" /><br /><br />
+                        <TextField className={classes.inputField} id="outlined-error" label="Aadhaar" variant="outlined" placeholder="Enter your Aadhar" /><br /><br />
+                        <TextField className={classes.inputField} id="outlined-error" label="GSTIN" variant="outlined" placeholder="Enter your GSTIN" /><br /><br />
+                        <TextField className={classes.inputField} id="outlined-error" type="password" label="Password" variant="outlined" placeholder="Enter your Password" /><br /><br />
+                        <TextField className={classes.inputField} id="outlined-error" type="password" label="Confirm Password" variant="outlined" placeholder="Enter your Password Again" /><br /><br />
+                    </form>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button className={classes.cardButton} size="large" color="secondary">
+                    SignUp
+                </Button>
+            </CardActions>
+        </Card>
+    );
 }
