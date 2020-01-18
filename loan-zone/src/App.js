dd80c66bb030de60c4ddaa1af979c39f01b6ebff
Akhil Bidhuri <abidhuri@cisco.com>
2020-01-19 00:49:15 +0530

    router and files

--- loan-zone/src/App.js
+++ loan-zone/src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import './App.css';
-import SignUp from './components/signup/signup'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import SignUp from './components/signup/signup';
+import Login from './components/login'
+import Landing from './components/landing'
+import Home from './components/home'
+import Err from './components/err'
 
 function App() {
   return (
-    <div className="App">
-      <p>test</p>
+    <Router>
+    <Switch>
+      <Route path="/" exact component={Landing}/>
+      <Route path="/login" component={SignUp}/>
+      <Route path="/login" component={Login}/>
+      <PrivateRoute path="/home" isAuthenticated={isAuthenticated}  Component={Home}/> 
+      <Route component={Err}/>
+    </Switch>
+  </Router>
+    // {<div className="App">
+    //   <p>test</p>
 
-        <SignUp />
+    //     <SignUp />
 
-    </div>
+    // </div>}
   );
 }
 
---
+++ loan-zone/src/components/err.js
@@ -0,0 +1,5 @@
+const Err = () => {
+    return ( <></> );
+}
+ 
+export default Err;
