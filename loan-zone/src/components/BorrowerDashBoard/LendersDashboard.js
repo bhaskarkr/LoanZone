4388850ed4f2c13fc62c1aa9830ca1a87e9fb1ab
Bhaskar Kumar <bhaskar.kumar@phonepe.com>
2020-01-19 03:17:34 +0530

    fixed

--- loan-zone/src/components/BorrowerDashBoard/LendersDashboard.js
+++ loan-zone/src/components/BorrowerDashBoard/LendersDashboard.js
@@ -12,31 +12,15 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import axios from 'axios';
 
 const useStyles = makeStyles({
-
+    
 });
 
-export default function borrowerDashBoard() {
+export default function SignUp() {
     const classes = useStyles();
+    
+   
 
-    const list = [
-        {
-            amount:1000,
-            lenderName: "Akanksh",
-            lenderPhoneNumber: "1234567890",
-            rate: 5,
-            borrowdate: Date.now(),
-            duration: 12
-        },
-        {
-            amount:5000,
-            lenderName: "Akhil",
-            lenderPhoneNumber: "0987654321",
-            rate: 7,
-            borrowdate: Date.now(),
-            duration: 5
-        }
-    ]
     return (
-        <p>sdfsfsd</p>
+        <p>asdas</p>
     );
 }
